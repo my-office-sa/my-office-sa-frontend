f49f4fff6bfa6acccde1aec194aef0571cb4e218
MARIA PILAR APONTE <maria_aponte@estudante.sesisenai.org.br>
2024-11-07 21:00:34 -0300

    estilização

--- src/comum/componentes/Cabecalho/Cabecalho.jsx
+++ src/comum/componentes/Cabecalho/Cabecalho.jsx
@@ -5,7 +5,7 @@ import Menu from "../Menu/Menu"
 function Cabecalho() {
   return (
     <header className="cabecalho_root">
-      <img src="/vite.svg" height={40} />
+      <img src="logo.png" height={40} />
      <Menu></Menu> 
     </header>
   );
