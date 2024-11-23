3923423caaaf7f8859ec1178d6fedc1994752c08
Glauberty Ribeiro <glaubertyribeiro007@gmail.com>
2024-11-23 20:45:01 -0300

    subindo rodapé customizado

--- src/comum/componentes/Rodape/Rodape.jsx
+++ src/comum/componentes/Rodape/Rodape.jsx
@@ -1,42 +1,12 @@
-<<<<<<< HEAD
+
 import FooterResponsivo from '../FooterResponsivo/FooterResponsivo';
 
 function Rodape() {
-
-  return <FooterResponsivo/>
-=======
-import { Link } from 'react-router-dom';
-import './Rodape.css';
-
-function Rodape() {
-  const anoAtual = new Date().getFullYear();
+ 
   return (
-    <footer className="rodape_root">   
+    <FooterResponsivo/> 
+   );
 
-      <div className="rodape_informacoes">
-        <p><strong>MyOffice {anoAtual}</strong></p>
-        <p>Soluções práticas para suas reuniões, em qualquer lugar.</p>
-      </div>
-      <div className='rodape_links'>
-        <a href="/about">Sobre Nós</a>
-        <a href="/servicos">Nossos Serviços</a>
-        <a href="/contatos">Contato</a>
-      </div>
-      <div className="rodape_redes_sociais">
-        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-          Facebook
-        </a>
-        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
-          Instagram
-        </a>
-      </div>
-      <div className="rodape_links">
-      <a href="/politica-privacidade">Política de Privacidade</a>
-      <a href="/termo-uso">Termos de Uso</a>
-      </div>
-    </footer>
-  );
->>>>>>> 3fd415475b67f9c053c4f247784c7ac7c39e0bf0
 }
 
 export default Rodape;
